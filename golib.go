76605f0e949765bf0b0c41dd25f181584c49b2e8
CloudChen <cloudchen2015@gmail.com>
2020-07-20 16:54:37 +0800

    Add GetVersion

--- golib.go
+++ golib.go
@@ -9,6 +9,7 @@ import (
 // GoLibrary golang library service
 type GoLibrary interface {
 	GetServMiscellaneous() miscellaneous.Miscellaneous
+	GetVersion() string
 	NewLogger(loggerFile, loggerLevel string) (logger.Logger, logger.Defer, error)
 	NewRotationLogger(loggerFile, loggerLevel string) (logger.Logger, error)
 	NewWeb(l logger.Logger, conf irisweb.Configure) (irisweb.Web, error)
